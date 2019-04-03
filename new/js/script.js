1a6c07851e1bca904b7fc1e4a59f64ad2e8b3ce5
Anirudh Goel <anirudhgoel.delhi@gmail.com>
2019-04-03 17:48:12 +0530

    Move all Javascript to new script.js file

---
+++ new/js/script.js
@@ -0,0 +1,71 @@
+/*
+Responsiveness for Wow animation
+*/
+function changeAnimation() {
+	var width = $(window).width();
+	if(width < 768) {
+		$(".wow").removeClass("fadeInUp fadeInLeft fadeInRight fadeIn fadeInUp").addClass("fadeInUp");
+	}
+}
+// Call function on page load
+changeAnimation();
+// Call WOW.js
+new WOW().init();
+
+
+
+/*
+Animation for Navbar color and size
+*/ 
+$(window).on("scroll load", function() {
+	if ($(document).scrollTop() > 0) {
+		$('nav').addClass('shrinkNav');
+	} else {
+		$('nav').removeClass('shrinkNav');
+	}
+});
+
+function autoScroll(sectionClass) {
+	$('html, body').animate({
+		scrollTop: $("." + sectionClass).offset().top
+	}, 750);
+}
+
+
+
+/*
+Loader animation
+*/
+function showLoader()
+{
+    $(".loader").fadeIn("slow");
+}
+function hideLoader()
+{
+    $(".loader").fadeOut("slow");
+}
+
+
+
+/*
+Vivus for name animation
+*/
+var viv = new Vivus('vivus-name', {
+	type: 'delayed',
+	duration: 150,
+	file: '../img/Anirudh Goel J.svg',
+	pathTimingFunction: Vivus.EASE
+}, mycallback);
+
+function mycallback() {
+	viv.stop()
+}
+
+
+
+hideLoader();
+
+
+
+
+console.log("%cHello! You have opened the console of my website.", "color: indigo");
